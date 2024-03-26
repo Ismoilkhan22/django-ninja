159353c2c9912e9347ae55bd49a0e3c3e2a5fdee
Ismoilkhan22 <ismoilkhan2205@gmail.com>
2024-03-26 23:52:55 +0500

    kichik misol apisi django ninjada

--- core/models.py
+++ core/models.py
@@ -2,3 +2,4 @@ from django.db import models
 
 class Product(models.Model):
     title = models.CharField(max_length=128)
+    
--- src/urls.py
+++ src/urls.py
@@ -15,10 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from ninja import NinjaAPI
+
+api = NinjaAPI()
+
+
+@api.get("/add")
+def add(request, a: int, b: int):
+    return {"result": a + b}
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/ninja/', include('core.urls'))
-    
+    path("admin/", admin.site.urls),
+    path("api/", api.urls),
 ]
